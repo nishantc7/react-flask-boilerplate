19c0cfc6e6384680d2d58e5e6365e5bea71de560
Nishant Choudhary <n1sh4ntch0udh4ry@gmail.com>
2020-05-23 05:37:57 +0530

    use blueprint structure

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,8 +1,10 @@
 from flask.cli import FlaskGroup
-from project import app, db
+from project import create_app, db
+# from project.api.models import User
 import unittest
 
-cli = FlaskGroup(app)
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
 
 
 @cli.command()
--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -2,35 +2,25 @@ import os
 from flask import Flask, jsonify
 # import datetime
 from flask_sqlalchemy import SQLAlchemy
+from project.api.users import users_blueprint
 
-
-app = Flask(__name__)
-
-# Set Configuration
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
 
 # Database Config
-db = SQLAlchemy(app)
-
-# model
+db = SQLAlchemy()
 
 
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+def create_app(script_info=None):
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+    app = Flask(__name__)
 
+    # Set Configuration
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
+    # set up extensions
+    db.init_app(app)
+    # register blueprints
+    app.register_blueprint(users_blueprint)
+    # Shell context
+    app.shell_context_processor({'app': app, 'db': db})
+    return app
