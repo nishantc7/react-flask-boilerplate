fb6b7a88f0439dad83c0a6b3e0c6afc582d49ba8
Nishant Choudhary <n1sh4ntch0udh4ry@gmail.com>
2020-05-28 02:27:26 +0530

    handle inputs - handle submit - add user creation

--- services/client/src/components/__tests__/UserList.test.jsx
+++ services/client/src/components/__tests__/UserList.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
+import renderer from 'react-test-renderer';
 import UsersList from '../UsersList';
 
 const users = [
@@ -22,4 +22,9 @@ test('UsersList renders properly', () => {
     const element = wrapper.find('h4');
     expect(element.length).toBe(2);
     expect(element.get(1).props.children).toBe('nishant');
+    });
+
+test('UsersList renders a snapshot properly', () => {
+    const tree = renderer.create(<UsersList users={users}/>).toJSON();
+    expect(tree).toMatchSnapshot();
     });
--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,54 +1,74 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
+
 
 class App extends Component {
-
   constructor() {
     super();
-    this.State = {
-      users: []
+    this.state = {
+      users: [],
+      username: '',
+      email: '',
     };
+    this.addUser = this.addUser.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   };
   componentDidMount() {
     this.getUsers();
-  }
+  };
   getUsers() {
     axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
     .then((res) => { this.setState({ users: res.data.data.users }); })
-    .catch((err) => {console.log(err);})
-  
-  }
+    .catch((err) => { console.log(err); });
+  };
+  addUser(event) {
+    event.preventDefault();
+    const data = {
+      username: this.state.username,
+      email: this.state.email
+    };
+    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+    .then((res) => {
+      this.getUsers();  // new
+      this.setState({ username: '', email: '' });  // new
+    })
+    .catch((err) => { console.log(err); });
+  };
+  handleChange(event) {
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  };
   render() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6">
-          <br/>
-          <h1>All Users</h1>
-          <hr/><br/>
-          <UsersList users={this.state.users}/>
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-half">
+              <br/>
+              <h1 className="title is-1">All Users</h1>
+              <hr/><br/>
+              <AddUser
+                username={this.state.username}
+                email={this.state.email}
+                addUser={this.addUser}
+                handleChange={this.handleChange}
+              />
+              <br/><br/>
+              <UsersList users={this.state.users}/>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  )
-    }
-  
-
-  
-  
+      </section>
+    )
+  }
 };
 
-
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
