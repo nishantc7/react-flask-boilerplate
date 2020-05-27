20f120ed60d3c5f9c420f26140062c0c6a6048c7
Nishant Choudhary <n1sh4ntch0udh4ry@gmail.com>
2020-05-28 01:05:51 +0530

    - add testing with jest and enzyme - add flask cors

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -2,20 +2,34 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
+import UsersList from './components/UsersList';
 
 class App extends Component {
 
   constructor() {
     super();
+    this.State = {
+      users: []
+    };
+  };
+  componentDidMount() {
+    this.getUsers();
   }
-  render(){
+  getUsers() {
+    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+    .then((res) => { this.setState({ users: res.data.data.users }); })
+    .catch((err) => {console.log(err);})
+  
+  }
+  render() {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-md-4">
+        <div className="col-md-6">
           <br/>
           <h1>All Users</h1>
           <hr/><br/>
+          <UsersList users={this.state.users}/>
         </div>
       </div>
     </div>
@@ -23,13 +37,9 @@ class App extends Component {
     }
   
 
-  getUsers() {
-    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
-    .then((res) => {console.log(res);})
-    .catch((err) => {console.log(err);})
-  }
+  
+  
 };
-
 
 
 ReactDOM.render(
